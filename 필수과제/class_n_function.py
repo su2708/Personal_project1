10657b994a6a6660e5ec70ed378f0a2ba1678499
su2708 <46125751+su2708@users.noreply.github.com>
2024-10-08 09:53:31 +0900

    Update class_n_function.py
    나이 입력 에러 처리

--- 필수과제/class_n_function.py
+++ 필수과제/class_n_function.py
@@ -9,10 +9,20 @@ class Person:
         print(f'나이: {self.age}')
         
 def main():
-    age = int(input('나이: '))
-    name = input('이름: ')
-    gender = input('성별: ')
-    
+    while True:
+        try:
+            age = int(input('나이: '))
+            if age <= 0 or age > 150:
+                raise ValueError('1 이상 150 이하 사이의 값으로 입력하세요.')
+            name = input('이름: ')
+            gender = input('성별: ')
+            break
+        # 잘못된 나이를 입력할 경우
+        except ValueError as ve:
+            print(f'잘못된 나이 입력입니다: {ve}')
+        except Exception as e:
+            print(f'에러 발생: {e}')
+        
     person = Person(name, gender, age)
     person.display()
 
